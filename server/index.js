12ce3403d4fc6b72118f91643f881b037f3d0c89
bungearnss <love2554f@hotmail.com>
2023-01-28 14:22:06 +0700

    Add node.js server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routers/auth");
+
+// INIT
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://mongodb:Password@cluster0.j4emwd2.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(authRouter);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT} hello`);
+});
+
+// Database connection
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
