324c68ba4ce2a720baf21c9e6f442ffa4a1d22d1
bungearnss <love2554f@hotmail.com>
2023-01-31 19:18:59 +0700

    Update AccountScreen

--- lib/src/features/account/screens/account_screen.dart
+++ lib/src/features/account/screens/account_screen.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
 
-class AccountScreen extends StatefulWidget {
+import '../../../utils/constants/app_color.dart';
+
+import '../widgets/header.dart';
+import '../widgets/render_buttons.dart';
+import '../widgets/orders.dart';
+
+class AccountScreen extends StatelessWidget {
   const AccountScreen({super.key});
 
   @override
-  State<AccountScreen> createState() => _AccountScreenState();
-}
-
-class _AccountScreenState extends State<AccountScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Account Screen"),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: AppColor.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: const [
+          Hearder(),
+          SizedBox(height: 10),
+          RenderButtons(),
+          SizedBox(height: 20),
+          Orders(),
+        ],
+      ),
     );
   }
 }
