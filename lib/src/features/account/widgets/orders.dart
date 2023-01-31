a9d3b501174b280be22e0178c129d5d38b31d827
bungearnss <love2554f@hotmail.com>
2023-01-31 19:19:41 +0700

    Add widgets for account screen

---
+++ lib/src/features/account/widgets/orders.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/order.dart';
+import '../services/account_services.dart';
+import '../../../utils/constants/app_color.dart';
+import '../../order_details/screens/order_detail_screen.dart';
+import '../widgets/product_item.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({super.key});
+
+  @override
+  State<Orders> createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  List<Order>? orders;
+  final AccountServices accountServices = AccountServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
+
+  void fetchOrders() async {
+    orders = await accountServices.fetchMyOrders(context: context);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return orders == null
+        ? const CircularProgressIndicator()
+        : Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                    ),
+                    child: const Text(
+                      'Your Orders',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      right: 15,
+                    ),
+                    child: Text(
+                      'See all',
+                      style: TextStyle(
+                        color: AppColor.selectedNavBarColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // display orders
+              Container(
+                height: 170,
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  top: 20,
+                  right: 0,
+                ),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: orders!.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(
+                          context,
+                          OrderDetailScreen.routeName,
+                          arguments: orders![index],
+                        );
+                      },
+                      child: ProductItem(
+                        image: orders![index].products[0].images[0],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+  }
+}
---
+++ lib/src/features/account/widgets/render_buttons.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import './account_button.dart';
+import '../services/account_services.dart';
+
+class RenderButtons extends StatelessWidget {
+  const RenderButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            AccountButton(
+              text: 'Your Orders',
+              onTap: () {},
+            ),
+            AccountButton(
+              text: 'Turn Seller',
+              onTap: () {},
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            AccountButton(
+              text: 'Log Out',
+              onTap: () => AccountServices().logOut(context),
+            ),
+            AccountButton(
+              text: 'Your Wish List',
+              onTap: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
