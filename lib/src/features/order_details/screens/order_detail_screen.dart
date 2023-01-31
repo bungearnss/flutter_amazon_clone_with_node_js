a75c4e146f99cfbc37a53604f0c095fe50040b2f
bungearnss <love2554f@hotmail.com>
2023-01-31 19:20:59 +0700

    Add order detail screen for navigation method

---
+++ lib/src/features/order_details/screens/order_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OrderDetailScreen extends StatefulWidget {
+  static const String routeName = '/order-detail';
+  const OrderDetailScreen({super.key});
+
+  @override
+  State<OrderDetailScreen> createState() => _OrderDetailScreenState();
+}
+
+class _OrderDetailScreenState extends State<OrderDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
