33cb9fc26670f4ad64532b2c674848917edfcdc8
bungearnss <love2554f@hotmail.com>
2023-02-01 20:14:55 +0700

    Add screen for "admin" user type

---
+++ lib/src/features/admin/screens/AnalyticScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnalyticScreen extends StatefulWidget {
+  const AnalyticScreen({super.key});
+
+  @override
+  State<AnalyticScreen> createState() => _AnalyticScreenState();
+}
+
+class _AnalyticScreenState extends State<AnalyticScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/features/admin/screens/admin_screen.dart
@@ -0,0 +1,141 @@
+import 'package:amazon_clone_with_node_js/src/models/order.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/app_color.dart';
+
+import './AnalyticScreen.dart';
+import './order_screen.dart';
+import './post_screen.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({super.key});
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  List<Widget> pages = [
+    const PostScreen(),
+    const AnalyticScreen(),
+    const OrderScreen(),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: AppColor.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              const Text(
+                'Admin',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: AppColor.selectedNavBarColor,
+        unselectedItemColor: AppColor.unselectedNavBarColor,
+        backgroundColor: AppColor.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        items: [
+          // POSTS
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 0
+                        ? AppColor.selectedNavBarColor
+                        : AppColor.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.home_outlined,
+              ),
+            ),
+            label: '',
+          ),
+          // ANALYTICS
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 1
+                        ? AppColor.selectedNavBarColor
+                        : AppColor.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.analytics_outlined,
+              ),
+            ),
+            label: '',
+          ),
+          // ORDERS
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 2
+                        ? AppColor.selectedNavBarColor
+                        : AppColor.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.all_inbox_outlined,
+              ),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
