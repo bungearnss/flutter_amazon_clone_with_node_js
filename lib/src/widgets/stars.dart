d6fae9c31ca4434f0ce1b3466448148c6cbd33ad
bungearnss <love2554f@hotmail.com>
2023-02-04 18:15:16 +0700

    Refactor global widgets

--- lib/src/widgets/stars.dart
+++ lib/src/widgets/stars.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-import '../../../utils/constants/app_color.dart';
+import '../utils/constants/app_color.dart';
 
 class Stars extends StatelessWidget {
   final double rating;
